3024a8b5a0125749cb23f5b001d18c64f1e053cc
JuliaLypchuk <yuliya.lypchuk@gmail.com>
2018-12-08 00:02:42 +0200

    add TODOs

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
@@ -9,7 +10,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class LandingComponent implements OnInit {
 
   spotifyLink: string = '';
-  trustedURL: any;
+  trustedURLs: any[] = [];
 
   filters: any = [
     'danceability',
@@ -18,15 +19,38 @@ export class LandingComponent implements OnInit {
     'loudness'
   ];
 
+  f1 = new FormControl(''); // TODO finish
+
+  leftText: string;
+  rightText: string;
+
+
   constructor(public sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
+    this.leftText = 'find your music';
+    this.rightText = 'and be happy';
+
     this.getSpotifyLink();
+    this.initForm();
   }
 
   getSpotifyLink() {
+    // TODO finish
     this.spotifyLink = 'https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3;';
-    this.trustedURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink);
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+  }
+
+  initForm() {
+// TODO finish
+  }
+
+  onSubmit(data: any) {
+    this.rightText = 'you click on button';
+    console.log(data);
   }
 }
