f61b546018e30815e42fcce568a9c18e9ff409c5
JuliaLypchuk <yuliya.lypchuk@gmail.com>
2018-12-07 23:13:45 +0200

    Merge branch 'master' of /home/yuliyalypchuk/WebstormProjects/MoodMusic/MoodMusic with conflicts.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: [ './landing.component.css' ]
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  spotifyLink: string = '';
+  trustedURL: any;
 
-  ngOnInit() {
+  filters: any = [
+    'f1',
+    'f2',
+    'f3',
+    'f4',
+  ];
+
+  constructor(public sanitizer: DomSanitizer) {
   }
 
+  ngOnInit() {
+    this.getSpotifyLink();
+  }
+
+  getSpotifyLink() {
+    this.spotifyLink = 'https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3;';
+    this.trustedURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink);
+  }
 }
