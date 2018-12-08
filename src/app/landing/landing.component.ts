bac86f2e0875c64f129c5a538402c6dce45a929f
JuliaLypchuk <yuliya.lypchuk@gmail.com>
2018-12-08 16:28:19 +0200

    change default params

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -21,6 +21,8 @@ export class LandingComponent implements OnInit {
   filters: any;
   moodParams: MoodParamsModel;
 
+  responseData: any;
+
   moodParamsForm: FormGroup;
 
   leftText: string;
@@ -31,6 +33,8 @@ export class LandingComponent implements OnInit {
     'relaxing',
     'calm'
   ];
+
+  lorenIpsum: string;
 
   constructor(public sanitizer: DomSanitizer, private http: HttpClient) {
 
@@ -44,20 +48,17 @@ export class LandingComponent implements OnInit {
     ];
 
     this.leftText = 'find your music';
+
+    this.lorenIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+      'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ' +
+      'veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ' +
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   }
 
   ngOnInit() {
     this.getSpotifyLink();
     this.initForm();
-  }
-
-  getSpotifyLink() {
-    // TODO finish
-    this.spotifyLink = 'https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3;';
-    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
-    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
-    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
-    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
   }
 
   initForm() {
@@ -70,16 +71,15 @@ export class LandingComponent implements OnInit {
     });
   }
 
-  prepareData() {
-    this.http.post('https://demo-mood-music.herokuapp.com/', this.moodParams)
-        .subscribe((res) => {
-          if ( res ) {
-            console.log(res);
-          }
-        }, error => {
-          alert(error);
-        });
+  prepareData(form: any) {
 
+    this.moodParams.text = form.text ? form.text : this.moodParams.text; // TODO set here smth more interest
+    this.moodParams.danceability = form.danceability ? form.danceability / 100 : 0;
+    this.moodParams.acousticness = form.acousticness ? form.acousticness / 100 : 0;
+    this.moodParams.instrumentalness = form.instrumentalness ? form.instrumentalness / 100 : 0;
+    this.moodParams.loudness = form.loudness ? form.loudness / 100 : 0;
+
+    console.log('', this.moodParams);
   }
 
   onSubmit(data: any) {
@@ -89,8 +89,28 @@ export class LandingComponent implements OnInit {
       behavior: 'smooth'
     });
 
-    console.log(this.moodParamsForm);
+    this.prepareData(this.moodParamsForm.value);
+    this.sendRequest();
+  }
 
-    this.prepareData();
+  sendRequest() {
+    this.http.post('https://demo-mood-music.herokuapp.com/', this.moodParams)
+        .subscribe((res) => {
+          if ( res ) {
+            this.responseData = res;
+            console.log(res);
+          }
+        }, error => {
+          alert(error);
+        });
+  }
+
+  getSpotifyLink() {
+    // TODO finish
+    this.spotifyLink = 'https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3;';
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
+    this.trustedURLs.push(this.sanitizer.bypassSecurityTrustResourceUrl(this.spotifyLink));
   }
 }
--- src/app/models/mood-params.model.ts
+++ src/app/models/mood-params.model.ts
@@ -6,10 +6,18 @@ export class MoodParamsModel {
   loudness?: number;
 
   constructor() {
-    this.text = 'happiness';
-    this.danceability = 0.5;
-    this.acousticness = 0.5
-    this.instrumentalness = 0.5;
-    this.loudness = 0.5;
+    this.text = 'Perhaps it’s safe to say that most people want to be happy. ' +
+      'They want to actually enjoy being here in this big, crazy, confusing world. ' +
+      'But too many people struggle with being happy. They can’t seem to find happiness in life. ' +
+      'To them, it is something that doesn’t come naturally to a lot of people, to be quite blunt. ' +
+      'Fortunately, there are plenty of strategies people can use to create happiness, or at least strive for it. ' +
+      'It really depends on one’s conviction to be always be happy, to have gratitude even when it is difficult to find, ' +
+      'having the right people in one’s life, and a person having ' +
+      'a job – or at least a hobby – that they feel good about doing, ' +
+      'that somehow brings meaning, direction, and purpose into their lives.';
+    this.danceability = 0;
+    this.acousticness = 0
+    this.instrumentalness = 0;
+    this.loudness = 0;
   }
 }
